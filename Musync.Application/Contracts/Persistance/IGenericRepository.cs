fd86246187db3a7e5d38a877c8b1a5b4569f0939
lamelapablo <lamelapablo@gmail.com>
2025-10-04 16:36:28 -0300

    - add application project - add generic repository interface and implementation

---
+++ Musync.Application/Contracts/Persistance/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿using Musync.Domain.Common;
+
+namespace Musync.Application.Contracts.Persistance
+{
+    public interface IGenericRepository<T> where T : BaseEntity
+    {
+        Task<IReadOnlyList<T>> GetAllAsync();
+        Task<T?> GetByIdAsync(int Id);
+        Task<T> CreateAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}
---
+++ Musync.Persistance/Repositories/GenericRepository.cs
@@ -0,0 +1,46 @@
+﻿using Musync.Domain.Common;
+using Musync.Persistance.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+using Musync.Application.Contracts.Persistance;
+
+namespace Musync.Persistance.Repositories
+{
+    public sealed class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+    {
+        private readonly MusyncDbContext _dbContext;
+
+        public GenericRepository(MusyncDbContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+        public async Task<T> CreateAsync(T entity)
+        {
+            await _dbContext.AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _dbContext.Remove(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<IReadOnlyList<T>> GetAllAsync()
+        {
+            return await _dbContext.Set<T>().AsNoTracking().ToListAsync();
+        }
+
+        public async Task<T?> GetByIdAsync(int Id)
+        {
+            return await _dbContext.Set<T>().AsNoTracking().FirstOrDefaultAsync(e => e.Id == Id);
+        }
+
+        public async Task<T> UpdateAsync(T entity)
+        {
+            _dbContext.Update(entity);
+            await _dbContext.SaveChangesAsync();
+            return entity;
+        }
+    }
+}
